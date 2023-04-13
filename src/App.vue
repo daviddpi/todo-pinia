21665d2bb59285d31afdf7bbe0799349356ae904
daviddpi <piscopodavid97@gmail.com>
2023-04-13 16:19:37 +0200

    init todoList

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TodoAppVue from "./components/TodoApp.vue";
+</script>
+
+<template>
+  <TodoAppVue/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TodoFormVue from "./TodoForm.vue";
+import TodoListVue from "./TodoList.vue";
+</script>
+
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <TodoFormVue/>
+    <TodoListVue/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
